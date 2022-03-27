d0acf9bd3b077c2805d72865cb9ade506e36c95c
jensie <jensvloon@gmail.com>
2022-03-27 14:06:19 +0200

    Hot reloading, fix for node-gyp build err

--- src/main.js
+++ src/main.js
@@ -1,70 +1,70 @@
 /**
-* system: Discord RPCTool
-* author: dekitarpg@gmail.com
-*/
+ * system: Discord RPCTool
+ * author: dekitarpg@gmail.com
+ */
 
 // set true to show dev consoles
-const SHOW_DEBUG = false; 
+const SHOW_DEBUG = false;
 
 /**
-* ■ Various Requirements:
-*/
-const {app, dialog, ipcMain, BrowserWindow, Menu, Tray} = require('electron');
+ * ■ Various Requirements:
+ */
+const { app, dialog, ipcMain, BrowserWindow, Menu, Tray } = require('electron');
 const DataStore = require('./libs/data-store');
 const path = require('path');
 const HTML_PATH = path.join(__dirname, 'app.html');
 const ICON_PATH = path.join(__dirname, 'img/rpc-icon.ico');
 const PRELOAD_PATH = path.join(__dirname, 'preload.js');
-const MAIN_WINDOW_SIZE = {w: 1024, h: 574};
-const CHILD_WINDOW_SIZE = {w: 640, h: 420};
+const MAIN_WINDOW_SIZE = { w: 1024, h: 574 };
+const CHILD_WINDOW_SIZE = { w: 640, h: 420 };
 // DEKRPC stores the running app windows/tray:
-const DEKRPC = {tray:null, main:null, child:null};
+const DEKRPC = { tray: null, main: null, child: null };
 // Application variable configuration 
-const app_config = new DataStore({  // DONT REMOVE!
-    filename: "[dekita.rpc.data]",  // DONT REMOVE!
-    defaults: {                     // DONT REMOVE!
-        'auto-boot': false,     // DONT REMOVE!
-        'auto-play': false,     // DONT REMOVE!
-        'auto-tiny': false,     // DONT REMOVE!
-        'tiny-tray': true,      // DONT REMOVE!
-    }                     // DONT REMOVE!
-});                       // DONT REMOVE!
+const app_config = new DataStore({ // DONT REMOVE!
+    filename: "[dekita.rpc.data]", // DONT REMOVE!
+    defaults: { // DONT REMOVE!
+        'auto-boot': false, // DONT REMOVE!
+        'auto-play': false, // DONT REMOVE!
+        'auto-tiny': false, // DONT REMOVE!
+        'tiny-tray': true, // DONT REMOVE!
+    } // DONT REMOVE!
+}); // DONT REMOVE!
 
 /**
-* ■ ipc handlers:
-*/
+ * ■ ipc handlers:
+ */
 ipcMain.handle('get-path', (event, key) => {
     if (key === 'app') return app.getAppPath();
     return app.getPath(key);
 });
-ipcMain.handle('openFileDialog', async (event) => {
-    const extensions = ['jpg','png','gif'];
+ipcMain.handle('openFileDialog', async(event) => {
+    const extensions = ['jpg', 'png', 'gif'];
     return await dialog.showOpenDialog({
-        filters: [{name: 'Images', extensions}],
+        filters: [{ name: 'Images', extensions }],
         properties: ['openFile'],
     });
 });
-ipcMain.handle("get-config", async (event, key) => {
+ipcMain.handle("get-config", async(event, key) => {
     return app_config.get(key);
 });
-ipcMain.handle("set-config", async (event, key, value) => {
+ipcMain.handle("set-config", async(event, key, value) => {
     const return_value = app_config.set(key, value);
     if (key === 'auto-boot') updateAutoBootMode();
     return return_value;
 });
-ipcMain.handle("delete-config", async (event, key) => {
+ipcMain.handle("delete-config", async(event, key) => {
     return app_config.delete(key);
 });
-ipcMain.handle("open-child-window", async (event, ...args) => {
+ipcMain.handle("open-child-window", async(event, ...args) => {
     return createChildWindow(...args);
 });
-ipcMain.handle("reload-child-window", async (event) => {
+ipcMain.handle("reload-child-window", async(event) => {
     return DEKRPC.child ? (DEKRPC.child.reload() || true) : false;
 });
 
 /**
-* ■ App/Windows Functions:
-*/
+ * ■ App/Windows Functions:
+ */
 
 // Create the main browser window.
 function createMainWindow() {
@@ -72,19 +72,21 @@ function createMainWindow() {
     DEKRPC.tray = null;
     let reloading = false;
     const width = MAIN_WINDOW_SIZE.w;
-    const height= MAIN_WINDOW_SIZE.h;
+    const height = MAIN_WINDOW_SIZE.h;
     DEKRPC.main = new BrowserWindow({
-        show: false, 
+        show: false,
         icon: ICON_PATH,
-        width, height,
+        width,
+        height,
         minWidth: width,
         minHeight: height,
-        autoHideMenuBar: true, 
+        autoHideMenuBar: true,
         useContentSize: true,
         backgroundColor: '#36393f',
         webPreferences: {
             preload: PRELOAD_PATH,
             contextIsolation: false,
+            nodeIntegration: true,
         },
     });
     DEKRPC.main.setMenu(null);
@@ -105,17 +107,17 @@ function createMainWindow() {
         event.preventDefault();
         // main_window.reload();
         reloading = true;
-    });  
+    });
     DEKRPC.main.on('closed', event => {
         if (DEKRPC.child) DEKRPC.child.close();
     });
-    DEKRPC.main.webContents.on('dom-ready', ()=>{
+    DEKRPC.main.webContents.on('dom-ready', () => {
         if (!reloading && app_config.get('auto-tiny')) {
             DEKRPC.main.minimize();
         } else {
             DEKRPC.main.show();
         }
-        if (SHOW_DEBUG){
+        if (SHOW_DEBUG) {
             DEKRPC.main.webContents.openDevTools();
         }
         reloading = false;
@@ -125,25 +127,25 @@ function createMainWindow() {
 // Creates a child window (not a true child) and accepts an html file path
 // to allow for easily opening additional windows/pages.
 // This is used for creating the app help guide window.
-function createChildWindow(html_path, width=null, height=null, debug=SHOW_DEBUG){
+function createChildWindow(html_path, width = null, height = null, debug = SHOW_DEBUG) {
     if (!width) width = CHILD_WINDOW_SIZE.w;
     if (!height) height = CHILD_WINDOW_SIZE.h;
     if (DEKRPC.child) return DEKRPC.child.reload();
     const main_bounds = DEKRPC.main.getBounds();
     DEKRPC.child = new BrowserWindow({
-        x: main_bounds.x + ((MAIN_WINDOW_SIZE.w - width)/2),
-        y: main_bounds.y + ((MAIN_WINDOW_SIZE.h - height)/2),
+        x: main_bounds.x + ((MAIN_WINDOW_SIZE.w - width) / 2),
+        y: main_bounds.y + ((MAIN_WINDOW_SIZE.h - height) / 2),
         // parent: DEKRPC.main,
         // modal: true,
         icon: ICON_PATH,
-        width: width, 
-        height: height, 
+        width: width,
+        height: height,
         minWidth: width,
         minHeight: height,
         autoHideMenuBar: true,
         useContentSize: true,
-        resizable: false, 
-        show:  false,
+        resizable: false,
+        show: false,
         webPreferences: {
             preload: PRELOAD_PATH,
             contextIsolation: false,
@@ -160,13 +162,16 @@ function createChildWindow(html_path, width=null, height=null, debug=SHOW_DEBUG)
 function createTray(windoe) {
     let tray_app = new Tray(ICON_PATH);
     const contextMenu = Menu.buildFromTemplate([
-        {label: 'Show', click: () => windoe.show()},
-        {label: 'Exit', click: () => {
-            app.isQuiting = true;
-            app.quit();
-        }},
-    ]); 
-    tray_app.on('double-click', function (event) {
+        { label: 'Show', click: () => windoe.show() },
+        {
+            label: 'Exit',
+            click: () => {
+                app.isQuiting = true;
+                app.quit();
+            }
+        },
+    ]);
+    tray_app.on('double-click', function(event) {
         windoe.show();
     });
     tray_app.setToolTip(windoe.title);
@@ -175,9 +180,9 @@ function createTray(windoe) {
 }
 
 // updates the 'auto-start at system boot' feature
-function updateAutoBootMode(){
+function updateAutoBootMode() {
     const openAtLogin = app_config.get('auto-boot');
-    app.setLoginItemSettings({openAtLogin});    
+    app.setLoginItemSettings({ openAtLogin });
 }
 
 // create window when electron has initialized.
@@ -197,3 +202,11 @@ app.on('activate', () => {
 
 // Keep running even if we hit unhandled rejections
 process.on('unhandledRejection', console.error);
+
+// Hot reloading
+if (SHOW_DEBUG) {
+    require('electron-reloader')(module, {
+        debug: true,
+        watchRenderer: true
+    });
+}
